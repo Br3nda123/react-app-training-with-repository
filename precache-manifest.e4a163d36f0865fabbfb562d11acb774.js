95db5e4a7fc2968aeac0d8fce184b6f2ff107db9
Brandon Kunicki <brandon.kunicki@gmail.com>
2020-10-22 18:00:25 +0200

    Updates

---
+++ precache-manifest.e4a163d36f0865fabbfb562d11acb774.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0bb177eb389fb1d41a12b92da41a6a0",
+    "url": "/react-app-training-with-repository/index.html"
+  },
+  {
+    "revision": "9aa6385ed77926e5fe86",
+    "url": "/react-app-training-with-repository/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "f8373a438c6aa6054957",
+    "url": "/react-app-training-with-repository/static/js/2.14e6cfe8.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react-app-training-with-repository/static/js/2.14e6cfe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aa6385ed77926e5fe86",
+    "url": "/react-app-training-with-repository/static/js/main.abb9dfa5.chunk.js"
+  },
+  {
+    "revision": "39a7d20694ef6eeba5c9",
+    "url": "/react-app-training-with-repository/static/js/runtime-main.61e2a2c8.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-app-training-with-repository/static/media/logo.ee7cd8ed.svg"
+  }
+]);
